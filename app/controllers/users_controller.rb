a13b4077212fd665062ab9f980e5d4f1e576446a
OwwllMan <doniermathieu@gmail.com>
2018-10-30 15:22:57 +0100

    GOOD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,21 @@ class UsersController < ApplicationController
   
   def create
   	@user = User.create(username: params[:username],email: params[:email],bio: params[:bio])
+    if !@user.valid?
+      redirect_to '/error'
+    else
+      redirect_to show_path username: @user.username
+    end
   end
 
   def show
   	@username = User.find_by(username: params[:username]).username
-  	@email = User.find_by(email: params[:email]).email
-  	@bio = User.find_by(bio: params[:bio]).bio
+  	@email = User.find_by(username: params[:username]).email
+  	@bio = User.find_by(username: params[:username]).bio
+  end
+
+  def fail
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+ 
 	root to: 'statique#accueil'
 	get 'users', to: 'users#new'
 	post 'users', to: 'users#create'
-	get 'users/:username', to: 'users#show'
-end
+	get 'users/:username', to: 'users#show', as: 'show'
+  get '/error', to: 'users#fail'
+
+  get '/home', to: 'statique#accueil'
+  get '/new', to: 'users#new'
+ end
